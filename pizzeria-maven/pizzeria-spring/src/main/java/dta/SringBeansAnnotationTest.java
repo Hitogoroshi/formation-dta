1a62b1320a7da959bc4dcbb0d85457ff4d10f29d
Charleen Kemps <kemps.charleen@gmail.com>
2016-09-23 09:17:51 +0200

    erreur precedantes retest

--- pizzeria-maven/pizzeria-spring/src/main/java/dta/SringBeansAnnotationTest.java
+++ pizzeria-maven/pizzeria-spring/src/main/java/dta/SringBeansAnnotationTest.java
@@ -1,10 +1,13 @@
 package dta;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import fr.pizzeria.service.Stockage;
 
+@RunWith(SpringJUnit4ClassRunner.class)
 public class SringBeansAnnotationTest {
 
 	@Test
