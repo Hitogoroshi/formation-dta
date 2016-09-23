16c4123a2d567bf883c7107e26a794a525f92c78
Charleen Kemps <kemps.charleen@gmail.com>
2016-09-23 09:27:02 +0200

    encore une fois

--- pizzeria-maven/pizzeria-spring/src/main/java/dta/SpringConfig.java
+++ pizzeria-maven/pizzeria-spring/src/main/java/dta/SpringConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("fr.pizzeria.service")
+@ComponentScan({ "dta", "fr.pizzeria.service" })
 public class SpringConfig {
 
 }
--- pizzeria-maven/pizzeria-spring/src/main/java/dta/SringBeansAnnotationTest.java
+++ pizzeria-maven/pizzeria-spring/src/main/java/dta/SringBeansAnnotationTest.java
@@ -2,21 +2,31 @@ package dta;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import fr.pizzeria.service.Stockage;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SpringConfig.class)
 public class SringBeansAnnotationTest {
+
+	@Autowired
+	private Stockage<?, ?> pizzaStockage;
+
+	@Autowired
+	private SpringConsole sc;
 
 	@Test
 	public void testCreationBean() {
 
-		try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class)) {
+		pizzaStockage.findAll().forEach(System.out::println);
+	}
 
-			Stockage<?, ?> pizzaStockage = context.getBean(Stockage.class);
-			pizzaStockage.findAll().forEach(System.out::println);
-		}
+	@Test
+	public void testSpringConsole() {
+
+		sc.afficherToutesLesPizzas();
 	}
 }
